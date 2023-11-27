d7ea2ca568812fae3aac39673dffe826b1b75e08
Sachin Malekar <sachinmalekar96gmail.com>
2023-11-27 22:57:36 +0530

    ngif  and ngtemplate learning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,8 @@ import { SuccessAlertComponent } from './success-alert/success-alert.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   // selector: '[app-servers]',
  selector: 'app-servers',
-  template: '<app-server></app-server>',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit{
+ allowNewServers = false;
+serverCreationsStatus="No server was created";
+serverName='TestServer';
+serverCreated=false;
+  constructor(){
+    setTimeout(()=>{
+      this.allowNewServers= true;
+    },2000);
+  }
 
+  ngOnInit(){
+  }
+
+  onCreateServer() {
+    this.serverCreated=true;
+    this.serverCreationsStatus =this.serverName+" server was created";
+  }
+
+  onUpdateServerName(event: Event){
+    this.serverName= (<HTMLInputElement>event.target).value;
+    console.log("event ", event);
+  }
 }
